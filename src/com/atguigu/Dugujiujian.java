598c27228758186af25e9cb768bf556c8606cee2
sujiawei1 <824098755@qq.com>
2018-01-31 19:24:06 +0800

    新增剑法

--- src/com/atguigu/Dugujiujian.java
+++ src/com/atguigu/Dugujiujian.java
@@ -5,6 +5,9 @@ public class Dugujiujian {
 	public static void main(String[] args) {
 		System.out.println("11111111111");
 		System.out.println("22222222222");
+		System.out.println("33333333333");
+		System.out.println("44444444444");
+		System.out.println("XXXXXXXXXXX");
 	}
 
 }
